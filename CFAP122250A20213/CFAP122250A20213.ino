9b92470672a305cb25c31724346eede9ba5c4d1a
trevinjorg <34657171+trevinjorg@users.noreply.github.com>
2021-09-08 14:47:39 -0700

    Update CFAP122250A20213.ino

--- CFAP122250A20213/CFAP122250A20213.ino
+++ CFAP122250A20213/CFAP122250A20213.ino
@@ -51,6 +51,7 @@
 // RESE -> 2.2ohms
 //=============================================================================
 //Connecting the Arduino to the SD card
+//A CFA10112 micro SD card adapter can be used: https://www.crystalfontz.com/product/cfa10112
 //
 // ARDUINO  |Wire Color |Function
 // ---------+-----------+--------------------
